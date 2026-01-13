bca9c42d5ebaed91349caa36d2a96911d18b604f
AvencastF <Avencast@fastmail.com>
2026-01-13 19:14:46 +0100

    add grid study

---
+++ data/Grid_Study/rsync_nersc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+base_dir="/pscratch/sd/t/tihsu/database/Grid_Study_CMS_OpenData_bbWW_HWW/method_arxiv"
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+rsync -Pavz \
+  --include='*/' \
+  --include='eval_metrics_*.json' \
+  --exclude='*' \
+  nersc:"$base_dir" "$script_dir"
